3fd2e337b69c171d4ddf948e80a95cbe33fb8cff
shiva <shivaprasadm613@gmail.com>
2021-08-21 21:54:08 +0530

    added signup component

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,11 +12,38 @@ const config = {
     messagingSenderId: "1026686139281",
     appId: "1:1026686139281:web:f9f4b74703e53901b23830",
     measurementId: "G-HL6YXH8EPV"
-  }; 
+};
 
-  firebase.initializeApp(config);
+
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if(!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+  const snapShot = await userRef.get();
+
+  if(!snapShot.exists){
+    const {displayName, email} = userAuth;
+
+    const createdAt = new Date();
+    console.log(snapShot.exists);
+    try{
+      await userRef.set({
+      displayName,email,createdAt,...additionalData
+    })
+  }catch(err){
+    console.log("error in creating user",err)
+  }
+
+
+  }
+
+  return userRef;
+} 
+
 //   firebase.analytics();
-
+firebase.initializeApp(config);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
